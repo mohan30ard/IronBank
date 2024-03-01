390a2cb4d7bd7ed671c44349809bbc1b95b82804
JohnShaize <70093395+JohnShaize@users.noreply.github.com>
2024-03-01 00:35:47 -0500

    Some ui changes

--- AccountCreationRequest.Designer.cs
+++ AccountCreationRequest.Designer.cs
@@ -54,9 +54,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label1.Location = new System.Drawing.Point(56, 44);
+            this.label1.Location = new System.Drawing.Point(457, 96);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(713, 45);
+            this.label1.Size = new System.Drawing.Size(355, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Please fill following details to create an account";
             // 
@@ -65,9 +65,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label2.Location = new System.Drawing.Point(152, 121);
+            this.label2.Location = new System.Drawing.Point(328, 194);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 37);
+            this.label2.Size = new System.Drawing.Size(53, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "Name :";
             // 
@@ -76,9 +76,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label3.Location = new System.Drawing.Point(138, 185);
+            this.label3.Location = new System.Drawing.Point(316, 258);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(129, 37);
+            this.label3.Size = new System.Drawing.Size(65, 19);
             this.label3.TabIndex = 2;
             this.label3.Text = "Address :";
             // 
@@ -87,9 +87,9 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label4.Location = new System.Drawing.Point(152, 250);
+            this.label4.Location = new System.Drawing.Point(326, 322);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(109, 37);
+            this.label4.Size = new System.Drawing.Size(55, 19);
             this.label4.TabIndex = 3;
             this.label4.Text = "Phone :";
             // 
@@ -98,9 +98,9 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label5.Location = new System.Drawing.Point(163, 325);
+            this.label5.Location = new System.Drawing.Point(331, 398);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(97, 37);
+            this.label5.Size = new System.Drawing.Size(50, 19);
             this.label5.TabIndex = 4;
             this.label5.Text = "Email :";
             // 
@@ -109,9 +109,9 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label6.Location = new System.Drawing.Point(523, 117);
+            this.label6.Location = new System.Drawing.Point(693, 190);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(82, 37);
+            this.label6.Size = new System.Drawing.Size(43, 19);
             this.label6.TabIndex = 5;
             this.label6.Text = "SSN :";
             // 
@@ -120,9 +120,9 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label7.Location = new System.Drawing.Point(474, 181);
+            this.label7.Location = new System.Drawing.Point(661, 254);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(146, 37);
+            this.label7.Size = new System.Drawing.Size(75, 19);
             this.label7.TabIndex = 6;
             this.label7.Text = "Password :";
             // 
@@ -131,9 +131,9 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label8.Location = new System.Drawing.Point(439, 253);
+            this.label8.Location = new System.Drawing.Point(637, 326);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(194, 37);
+            this.label8.Size = new System.Drawing.Size(99, 19);
             this.label8.TabIndex = 7;
             this.label8.Text = "Confirm-Pass :";
             // 
@@ -142,9 +142,9 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label9.Location = new System.Drawing.Point(435, 321);
+            this.label9.Location = new System.Drawing.Point(632, 394);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(201, 37);
+            this.label9.Size = new System.Drawing.Size(104, 19);
             this.label9.TabIndex = 8;
             this.label9.Text = "Initial Balance :";
             // 
@@ -152,30 +152,30 @@
             // 
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(233, 121);
+            this.textBox1.Location = new System.Drawing.Point(386, 194);
             this.textBox1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(158, 36);
+            this.textBox1.Size = new System.Drawing.Size(158, 18);
             this.textBox1.TabIndex = 10;
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Location = new System.Drawing.Point(233, 185);
+            this.textBox2.Location = new System.Drawing.Point(386, 258);
             this.textBox2.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(158, 36);
+            this.textBox2.Size = new System.Drawing.Size(158, 18);
             this.textBox2.TabIndex = 11;
             // 
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox3.Location = new System.Drawing.Point(233, 250);
+            this.textBox3.Location = new System.Drawing.Point(386, 323);
             this.textBox3.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(158, 36);
+            this.textBox3.Size = new System.Drawing.Size(158, 18);
             this.textBox3.TabIndex = 12;
             // 
             // textBox4
@@ -183,50 +183,50 @@
             this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
             this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.textBox4.Location = new System.Drawing.Point(233, 325);
+            this.textBox4.Location = new System.Drawing.Point(386, 398);
             this.textBox4.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(158, 36);
+            this.textBox4.Size = new System.Drawing.Size(158, 18);
             this.textBox4.TabIndex = 13;
             // 
             // textBox5
             // 
             this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
             this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox5.Location = new System.Drawing.Point(588, 118);
+            this.textBox5.Location = new System.Drawing.Point(741, 191);
             this.textBox5.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(158, 36);
+            this.textBox5.Size = new System.Drawing.Size(158, 18);
             this.textBox5.TabIndex = 14;
             // 
             // textBox6
             // 
             this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
             this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox6.Location = new System.Drawing.Point(588, 182);
+            this.textBox6.Location = new System.Drawing.Point(741, 255);
             this.textBox6.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(158, 36);
+            this.textBox6.Size = new System.Drawing.Size(158, 18);
             this.textBox6.TabIndex = 15;
             // 
             // textBox7
             // 
             this.textBox7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
             this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox7.Location = new System.Drawing.Point(588, 253);
+            this.textBox7.Location = new System.Drawing.Point(741, 326);
             this.textBox7.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(158, 36);
+            this.textBox7.Size = new System.Drawing.Size(158, 18);
             this.textBox7.TabIndex = 16;
             // 
             // textBox8
             // 
             this.textBox8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
             this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox8.Location = new System.Drawing.Point(588, 322);
+            this.textBox8.Location = new System.Drawing.Point(741, 395);
             this.textBox8.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(158, 36);
+            this.textBox8.Size = new System.Drawing.Size(158, 18);
             this.textBox8.TabIndex = 17;
             // 
             // button1
@@ -234,7 +234,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Yu Gothic", 8F);
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.button1.Location = new System.Drawing.Point(282, 412);
+            this.button1.Location = new System.Drawing.Point(435, 485);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(167, 52);
@@ -248,7 +248,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Yu Gothic", 8F);
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.button2.Location = new System.Drawing.Point(492, 412);
+            this.button2.Location = new System.Drawing.Point(645, 485);
             this.button2.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(167, 52);
@@ -259,7 +259,7 @@
             // 
             // AccountCreationRequest
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 37F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
             this.ClientSize = new System.Drawing.Size(1241, 642);
